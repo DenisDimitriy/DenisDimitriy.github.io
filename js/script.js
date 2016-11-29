635f3cf4901d58f14e439120e4a39f36d9674f94
DenisDimitriy <denis.dimitriy@gmail.com>
2016-11-29 21:37:11 +0200

    rotate photocard

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 /**
  * Created by Denis on 10/16/2016.
  */
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function () {
+        $('body,html').animate({scrollTop: 0}, 800);
+    });
+
+});
